81816c83a2eb990a0b260c7097222b76b115abe9
Marc Majcher <majcher@gmail.com>
2017-01-13 14:17:10 -0600

    ejs test

--- routes/index.js
+++ routes/index.js
@@ -11,29 +11,19 @@ router.get('/', (req, res) => {
 });
 
 router.post('/text', (req, res) => {
-  const intext = req.body.intext;
-  const wordCount = lex.wordCount(intext);
-  const charCount = lex.charCount(intext);
-  const charCountSpaces = lex.charCount(intext, {
-    noSpaces: true
-  });
-  const charCountPunctuation = lex.charCount(intext, {
-    noPunctuation: true
-  });
-  const mostFreq = lex.mostFreq(intext);
-  const leastFreq = lex.leastFreq(intext);
-  const summary = lex.summarize(intext);
-
-  res.render('result', {
-    intext,
-    charCount,
-    charCountSpaces,
-    charCountPunctuation,
-    wordCount,
-    mostFreq,
-    leastFreq,
-    summary
-  });
+  // const intext = req.body.intext;
+  // const wordCount = lex.wordCount(intext);
+  // const charCount = lex.charCount(intext);
+  // const charCountSpaces = lex.charCount(intext, {
+  //   noSpaces: true
+  // });
+  // const charCountPunctuation = lex.charCount(intext, {
+  //   noPunctuation: true
+  // });
+  // const mostFreq = lex.mostFreq(intext);
+  // const leastFreq = lex.leastFreq(intext);
+  // const summary = lex.summarize(intext);
+  res.send('Hook up ejs');
 });
 
 module.exports = router;
